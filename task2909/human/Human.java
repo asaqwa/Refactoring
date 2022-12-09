ad2cbd72fd68fada96308678e9611199b534eba3
Andrey <bursov@web.de>
2022-12-09 23:09:07 +0100

    Third task complete

--- task2909/human/Human.java
+++ task2909/human/Human.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Human {
+public class Human implements Alive {
     public static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
-    protected int course;
+
 
     private List<Human> children = new ArrayList<>();
 
@@ -50,10 +50,6 @@ public class Human {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getCourse() {
-        return course;
     }
 
     public List<Human> getChildren() {
