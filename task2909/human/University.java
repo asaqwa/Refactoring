aecff797d5e6c7de3fa124a93becc0744d44eeca
Andrey <bursov@web.de>
2022-12-11 09:45:45 +0100

    Sixth task complete

--- task2909/human/University.java
+++ task2909/human/University.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task29.task2909.human;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class University {
@@ -13,18 +14,21 @@ public class University {
         this.age = age;
     }
 
-    public Student getStudentWithAverageGrade() {
-        //TODO:
-        return null;
+    public Student getStudentWithAverageGrade(double averageGrade) {
+        return students.stream().filter(student -> student.getAverageGrade()==averageGrade)
+                .findFirst().orElse(null);
     }
 
-    public Student getStudentWithMaxAverageGrade(double averageGrade) {
-        //TODO:
-        return null;
+    public Student getStudentWithMaxAverageGrade() {
+        return students.stream().max(Comparator.comparingDouble(Student::getAverageGrade)).orElse(null);
     }
 
-    public void getStudentWithMinAverageGradeAndExpel() {
-        //TODO:
+    public Student getStudentWithMinAverageGrade() {
+        return students.stream().min(Comparator.comparingDouble(Student::getAverageGrade)).orElse(null);
+    }
+
+    public void expel(Student student) {
+        students.remove(student);
     }
 
     public String getName() {
