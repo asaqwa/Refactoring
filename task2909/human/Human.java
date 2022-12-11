78d735d1e50b0e729ca620c3a85233fe9864a4fa
Andrey <bursov@web.de>
2022-12-11 10:58:54 +0100

    Ninth task complete

--- task2909/human/Human.java
+++ task2909/human/Human.java
@@ -13,19 +13,15 @@ public class Human implements Alive {
 
     private List<Human> children = new ArrayList<>();
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
-    public int getBloodGroup() {
+    public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
 
@@ -80,6 +76,11 @@ public class Human implements Alive {
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
+    }
+
+    public class Size {
+        public int height;
+        public int weight;
     }
 }
